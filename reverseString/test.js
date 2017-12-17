2303f9993de551db9e5156b4e2d154e2fc64971e
nakanakano <the.fear.of.manju@gmail.com>
2017-12-17 18:59:09 +0900

    Create test.js

---
+++ reverseString/test.js
@@ -0,0 +1,22 @@
+//good
+function reverse(str) {
+   return str
+     .split('')
+     .reverse()
+     .join('');
+}
+//better
+function reverse(str) {
+  let reversed = '';
+
+  for (let character of str) {
+     reversed = character + reversed;
+   }
+
+  return reversed;
+}
+
+//best
+function reverse(str) {
+  return str.split('').reduce((rev, char) => char + rev, '');
+}
